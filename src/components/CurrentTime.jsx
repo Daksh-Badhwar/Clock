bf977f2583d89eeea3f03188d1b459a3c10e19ff
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-05-19 17:42:20 +0530

    BharatClock

---
+++ src/components/CurrentTime.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const CurrentTime=()=>{
+
+
+const time = new Date();
+
+return <p> This is the current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}  </p>;
+}
+export default CurrentTime;
